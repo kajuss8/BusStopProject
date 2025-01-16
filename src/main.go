088d66e8150f0f5f024e9d40a8357d135550962c
Kajus Sciaponis <kajus.sciaponis@gmail.com>
2025-01-16 16:18:31 +0200

    download and unzip gtfs

--- src/main.go
+++ src/main.go
@@ -1,9 +1,29 @@
 package main
 
-
-
-
+import (
+	// "busProject/src/models"
+	// "net/http"
+	
+	// "github.com/gin-gonic/gin"
+	"busProject/src/handleFiles"
+)
 
 func main() {
-	
+	// router := gin.Default()
+	// stops := models.CreateStops()
+	// router.GET("/tasks", func(ctx *gin.Context) {
+	// 	ctx.JSON(http.StatusOK, gin.H{"stops": stops})
+	// })
+
+	// router.Run()
+	err := handleFiles.DownloadGtfs()
+	if err != nil {
+		panic(err)
+	}
+
+	err = handleFiles.Unzip()
+	if err != nil {
+		panic(err)
+	}
+
 }
--- src/models/stop.go
+++ src/models/stop.go
@@ -11,7 +11,7 @@ const (
 )
 
 type Stop struct {
-	Id            uint16           `json:"Id"`
+	Id            uint16           `json:"id"`
 	StopId        string           `json:"stopId"`
 	StopCode      string           `json:"stopCode"`
 	StopName      string           `json:"stopName"`
@@ -21,4 +21,14 @@ type Stop struct {
 	StopUrl       string           `json:"stopUrl"`
 	LocationType  LocationCategory `json:"locationType"`
 	ParentStation uint16           `json:"parentStation"`
+}
+
+func CreateStops() []Stop {
+	return []Stop{
+		{1, "1", "1", "Stop 1", "Stop 1 Description", 1.0, 1.0, "http://stop1.com", StopOrPlatform, 0},
+		{2, "2", "2", "Stop 2", "Stop 2 Description", 2.0, 2.0, "http://stop2.com", StopOrPlatform, 0},
+		{3, "3", "3", "Stop 3", "Stop 3 Description", 3.0, 3.0, "http://stop3.com", EntranceExit, 0},
+		{4, "4", "4", "Stop 4", "Stop 4 Description", 4.0, 4.0, "http://stop4.com", StopOrPlatform, 0},
+		{5, "5", "5", "Stop 5", "Stop 5 Description", 5.0, 5.0, "http://stop5.com", Station, 0},
+	}
 }
