c97e6c81be3cdde2c092d64fc08159379eefe939
Kajus Sciaponis <kajus.sciaponis@gmail.com>
2025-01-20 10:34:06 +0200

    API calls

--- src/main.go
+++ src/main.go
@@ -4,17 +4,19 @@ import (
 	"busProject/src/handleFiles"
 	"busProject/src/models"
 	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 )
 
-func getArriveTimes(ctx *gin.Context) {
+func getTransportArrivalTimes(ctx *gin.Context) {
 	id := ctx.Param("id")
-	arriveTimes, err := models.GetArriveTimesById(id)
+	arriveTimes, err := models.GetArrivalTimes(id)
 	if err != nil {
 		ctx.JSON(http.StatusNotFound, gin.H{"error": "Stop not found"})
 		return
 	}
-	ctx.JSON(http.StatusOK, gin.H{"arrive times": arriveTimes})
+	ctx.JSON(http.StatusOK, gin.H{"arrival times": arriveTimes})
 }
 
 func getStopById(ctx *gin.Context) {
@@ -37,13 +39,47 @@ func getStopTimesById(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"stop Times": stopTimes})
 }
 
+func getStopName(ctx *gin.Context) {
+	id := ctx.Param("id")
+	stopTimes, err := models.GetStopName(id)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "stop not found"})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"stop name": stopTimes})
+}
+
+func getTripId(ctx *gin.Context) {
+	id := ctx.Param("id")
+	tripId, err := models.GetTripId(id)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "Stop not found"})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"tripId": tripId})
+}
+
+func getWeekWorkDays(ctx *gin.Context){
+	id := ctx.Param("id")
+	tempId, _ := strconv.Atoi(id)
+	tripId, err := models.GetCalendarWorkDays(tempId)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "No such service ID"})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"Wrok days": tripId})
+}
+
 func main() {
 
 	handleFiles.ProcessGtfs()
 
 	router := gin.Default()
+	router.GET("/tripId/:id", getTripId)
+	router.GET("/stopsName/:id", getStopName)
 	router.GET("/stopTimesById/:id", getStopTimesById)
-	router.GET("/arriveTimes/:id", getArriveTimes)
+	router.GET("/arrivalTimes/:id", getTransportArrivalTimes)
 	router.GET("/stop/:id", getStopById)
+	router.GET("/WorkDays/:id", getWeekWorkDays)
 	router.Run()
 }
